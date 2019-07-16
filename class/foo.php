4aba51cc5b08af1dc32ff935e3e07e41235cbc62
Test <portalprfst@gmail.com>
2019-07-17 00:07:26 +0300

    тестирорвание phpunit

---
+++ class/foo.php
@@ -0,0 +1,13 @@
+<?php
+namespace test;
+
+class Foo {
+    public $aMemberVar = 'aMemberVar Member Variable';
+    public $aFuncName = 'aMemberFunc';
+
+
+    function aMemberFunc() {
+        return 'Inside `aMemberFunc()`';
+    }
+}
+?>
